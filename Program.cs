8b6362d2f5b4d345beda6ebf97a3cdfe55b8446c
PolinaHolotsvan <115098500+PolinaHolotsvan@users.noreply.github.com>
2022-11-02 14:58:20 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,49 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
+[Serializable]
 class Book
 {
+    [JsonIgnore]
     public int PublishingHouseId { get; set; }
+    [JsonPropertyName("Name")]
     public string Title { get; set; }
     public PublishingHouse PublishingHouse { get; set; }
+    public Book(int id, string title, string name, string address)
+    {
+        PublishingHouseId = id;
+        Title = title;
+        PublishingHouse = new PublishingHouse(id, name, address);
+    }
 
 }
-
 
 class PublishingHouse
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Adress { get; set; }
+    public PublishingHouse(int id, string name, string address)
+    {
+        Name = name;
+        Id = id;
+        Adress = address;
+    }
 }
 class Program
 {
     public static async Task Main(string[] args)
     {
-        string jsonString = File.ReadAllText("tsconfig1.json");
-        
-        Console.WriteLine("The file"+jsonString);
-
-        var books = JsonSerializer.Deserialize<List<Book>>(jsonString);
-        Console.WriteLine("The names of each publishing house");
-        foreach (var book in books)
+        Book book1 = new Book(2, "Algebra 8", "Gymnasium", "Address 2");
+        Book book2 = new Book(1, "The diary of neurosurgeon", "Old Lion's Publishing house", "Address 1");
+        Book book3 = new Book(2, "Algebra 9", "Gymnasium", "Address 2");
+        List<Book> books = new List<Book>();
+        books.Add(book1);
+        books.Add(book2);
+        books.Add(book3);
+        using (FileStream fs = new FileStream("result.json", FileMode.OpenOrCreate))
         {
-            Console.WriteLine(book.Title);
+            await JsonSerializer.SerializeAsync<List<Book>>(fs, books);
         }
     }
 }
