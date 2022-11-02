502e8f933fdd7621da3ac5d8d9a47204dc9bbaf9
PolinaHolotsvan <swan.polina00@gmail.com>
2022-11-02 16:57:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+class Book
+{
+    public int PublishingHouseId { get; set; }
+    public string Title { get; set; }
+    public PublishingHouse PublishingHouse { get; set; }
+
+}
+
+
+class PublishingHouse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Adress { get; set; }
+}
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string jsonString = File.ReadAllText("tsconfig1.json");
+        
+        Console.WriteLine("The file"+jsonString);
+
+        var books = JsonSerializer.Deserialize<List<Book>>(jsonString);
+        Console.WriteLine("The names of each publishing house");
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.Title);
+        }
+    }
+}
